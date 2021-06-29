c2706b7a45611578d6044910e02ddb4770e63aad
Gabor Fekete <dev.feheren.fekete@gmail.com>
2021-06-29 12:58:27 +0300

    mess test.cpp

--- test.cpp
+++ test.cpp
@@ -185,9 +185,10 @@ void lambdas()
     {
         return true;
     };
-    auto lambda3 = [=](float f, int a)
+    auto lambda3 = [=](     float f, int       a)
     {
-        const auto r = f + a;
+        const auto r = f +
+a;
         return r;
     };
     functionCallWithLambda(
