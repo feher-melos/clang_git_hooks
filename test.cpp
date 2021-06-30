2f003d9f08fcfa73891c8eb78fcc740a35db568d
Gabor Fekete <dev.feheren.fekete@gmail.com>
2021-06-30 14:43:42 +0300

    mess test.cpp

--- test.cpp
+++ test.cpp
@@ -86,7 +86,10 @@ AppleDevice::AppleDevice(int apple,
 {
 }
 
-AppleDevice::AppleDevice(int a, std::string b) : apple{a}, banana{b}, strList{}
+AppleDevice::AppleDevice(int a, std::string b)
+    : apple{a},
+      banana{b},
+      strList{}
 {
 }
 
@@ -104,9 +107,7 @@ struct B
     {
         cout << "B" << endl;
     }
-    static A a1;
-    static A a2;
-    static A a3;
+    static A a1;  static A a2; static A a3;
 };
 
 A B::a2{2};
